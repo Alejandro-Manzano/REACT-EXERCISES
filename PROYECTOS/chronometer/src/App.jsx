efe1c3238907736df6520c069fe606b672d436af
Alejandro Manzano <alejandromanzano.laforga@gmail.com>
2023-06-03 18:19:18 +0200

    countdown

--- PROYECTOS/chronometer/src/App.jsx
+++ PROYECTOS/chronometer/src/App.jsx
@@ -1,11 +1,14 @@
 import './App.css'
+import DigitalClock from './components/DigitalClock/DigitalClock'
 
 function App() {
 
   return (
-    <>
+    <div>
       
-    </>
+      <DigitalClock></DigitalClock>
+
+    </div>
   )
 }
 
---
+++ PROYECTOS/chronometer/src/components/DigitalClock/DigitalClock.jsx
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from "react";
+
+const DigitalClock = () => {
+
+  const [clockState, setClockState] = useState();
+
+  useEffect(() => {
+
+    setInterval(() => {
+      const date = new Date(); // Date representa la fecha y hora actual 
+      setClockState(date.toLocaleTimeString()); // se utiliza para obtener la hora actual 
+      //y va actualizando cada segundo gracias al Set, así cada S clock cambia pq esta siendo actualizado
+    }, 1000); // se va a ejecutar cada segundo  
+
+  }, []);
+  //con el array vacío se ejecutara solo una y será cuando se monte
+  
+  return (
+    <div className="digital-clock">
+      <h2>{clockState}</h2>
+    </div>
+  );
+};
+
+export default DigitalClock;
